2ff0cf7798bf5c2be0a8d807e5f0f6d91aeafe58
yash57-ty <thummaryash969@gmail.com>
2026-07-12 09:24:40 +0530

    fix(deploy): proxy api requests through vercel to bypass mixed content blocks - Configure Vercel rewrite proxy to forward secure HTTPS requests to HTTP AWS Beanstalk - Restore Axios relative baseURL to /api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.PROD
-    ? 'http://cars247-env.eba-hdqdmw3r.ap-southeast-2.elasticbeanstalk.com/api'
-    : '/api',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
